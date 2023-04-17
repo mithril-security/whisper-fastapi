d4911e26ea939ba5adebcfecda99324ccd7db9e6
Lucas Bourtoule <lucas.bourtoule@mithrilsecurity.io>
2023-04-17 15:17:34 +0200

    packaging fix

--- parent_app.sh
+++ parent_app.sh
@@ -2,7 +2,7 @@
 
 python3 -m venv env
 source env/bin/activate
-pip install -r requirements.txt
+pip install -r parent_app_requirements.txt
 python model_store.py download "openai/whisper-tiny.en"
 python model_store.py download "togethercomputer/Pythia-Chat-Base-7B"
 exec python model_store.py serve
