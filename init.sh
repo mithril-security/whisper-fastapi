7898c1729b77b210bddf6bf7950f101ae0b0fd0d
shannon <shannonsaviodsouza@gmail.com>
2023-04-20 13:49:04 +0200

    added sleep for dockerd to start

--- init.sh
+++ init.sh
@@ -25,10 +25,10 @@ apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docke
 # Switch to legacy iptables 
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-sleep 10
 
 # Start docker service
 DOCKER_RAMDISK=true dockerd &
+sleep 15
 
 # Create network adapter
 docker network create --driver bridge proxynet
